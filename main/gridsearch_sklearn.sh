2ee280168ab6af937a97d39a16c5402949c31c25
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-12-07 00:30:34 +0900

    bash file for gridsearch machine learning models

---
+++ main/gridsearch_sklearn.sh
@@ -0,0 +1,4 @@
+python gridsearch_sklearn.py --feature imputed --model logistic --n_jobs 1
+python gridsearch_sklearn.py --feature imputed --model rf --n_jobs 1
+python gridsearch_sklearn.py --feature imputed --model svm --n_jobs -1
+python gridsearch_sklearn.py --feature imputed --model ann --n_jobs 1
