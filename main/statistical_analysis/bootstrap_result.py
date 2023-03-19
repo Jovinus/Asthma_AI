2c2c50a5c1e5c700d02a4e869b09d93cb2aea88d
KyungHyun Jovinus Lee <lkh256@gmail.com>
2023-03-19 20:10:10 +0900

    Refactor codes

--- main/statistical_analysis/bootstrap_result.py
+++ main/statistical_analysis/bootstrap_result.py
@@ -1,4 +1,3 @@
-# %%
 import os
 import pandas as pd
 import numpy as np
@@ -10,7 +9,7 @@ from sklearn.utils import resample
 
 pd.set_option("display.max_columns", None)
 
-# %%
+
 def get_ppv_npv(y_true, y_pred, option='ppv'):
     tp, fn, fp, tn = confusion_matrix(y_true, y_pred).ravel()
     ppv = (tp)/(tp+fp)
@@ -20,16 +19,15 @@ def get_ppv_npv(y_true, y_pred, option='ppv'):
     elif option == 'npv':
         return npv
 
-# %%
+
 def calculate_bootstrap_metric(
-    df_log:pd.DataFrame, 
-    target:str, 
-    predict:str, 
-    n_bootstrap:int,
-    model:str,
+    df_log: pd.DataFrame,
+    target: str,
+    predict: str,
+    n_bootstrap: int,
+    model: str,
 ) -> None:
 
-    
     acc = []
     f1 = []
     auroc = []
@@ -39,76 +37,75 @@ def calculate_bootstrap_metric(
     specificity = []
     ppv = []
     npv = []
-    
 
     for i in range(n_bootstrap):
         sampled_df = resample(
-            df_log, 
-            replace=True, 
-            n_samples=300, 
+            df_log,
+            replace=True,
+            n_samples=300,
             random_state=i
         )
-        
+
         calc_acc = accuracy_score(
-            sampled_df[target], 
-            sampled_df[predict], 
+            sampled_df[target],
+            sampled_df[predict],
         )
-        
+
         calc_precision = precision_score(
-            sampled_df[target], 
-            sampled_df[predict], 
+            sampled_df[target],
+            sampled_df[predict],
         )
-        
+
         calc_recall = recall_score(
-            sampled_df[target], 
-            sampled_df[predict], 
+            sampled_df[target],
+            sampled_df[predict],
         )
-        
+
         calc_specificity = specificity_score(
-            sampled_df[target], 
-            sampled_df[predict], 
+            sampled_df[target],
+            sampled_df[predict],
             pos_label=1,
         )
-        
+
         calc_f1 = f1_score(
-            sampled_df[target], 
-            sampled_df[predict], 
+            sampled_df[target],
+            sampled_df[predict],
             average="binary"
         )
-        
+
         calc_ppv = get_ppv_npv(
             sampled_df[target],
             sampled_df[predict],
             option='ppv'
         )
-        
+
         calc_npv = get_ppv_npv(
             sampled_df[target],
             sampled_df[predict],
             option='npv'
         )
-        
+
         if model == "conventional":
             calc_auroc = roc_auc_score(
-                sampled_df[target], 
-                sampled_df["MBPT_result"], 
+                sampled_df[target],
+                sampled_df["MBPT_result"],
             )
-            
+
             calc_auprc = average_precision_score(
-                sampled_df[target], 
-                sampled_df["MBPT_result"], 
+                sampled_df[target],
+                sampled_df["MBPT_result"],
             )
         else:
             calc_auroc = roc_auc_score(
-                sampled_df[target], 
-                sampled_df['pred_proba'], 
+                sampled_df[target],
+                sampled_df['pred_proba'],
             )
-            
+
             calc_auprc = average_precision_score(
-                sampled_df[target], 
-                sampled_df['pred_proba'], 
+                sampled_df[target],
+                sampled_df['pred_proba'],
             )
-        
+
         acc.append(calc_acc)
         precision.append(calc_precision)
         recall.append(calc_recall)
@@ -118,7 +115,7 @@ def calculate_bootstrap_metric(
         auprc.append(calc_auprc)
         ppv.append(calc_ppv)
         npv.append(calc_npv)
-        
+
     print(f"Accuracy = {np.mean(acc):.3f} +- {np.std(acc):.3f}")
     print(f"Precision = {np.mean(precision):.3f} +- {np.std(precision):.3f}")
     print(f"Recall(Sensitivity) = {np.mean(recall):.3f} +- {np.std(recall):.3f}")
@@ -128,33 +125,33 @@ def calculate_bootstrap_metric(
     print(f"F1 Score = {np.mean(f1):.3f} +- {np.std(f1):.3f}")
     print(f"AUROC = {np.mean(auroc):.3f} +- {np.std(auroc):.3f}")
     print(f"AUPRC = {np.mean(auprc):.3f} +- {np.std(auprc):.3f}\n")
-    
+
     metrics = {
         "accuracy": acc,
         "precision": precision,
         "recall": recall,
         "specificity": specificity,
-        "ppv":ppv,
-        "npv":npv,
-        "f1": f1, 
-        "auroc": auroc, 
-        "auprc": auprc, 
+        "ppv": ppv,
+        "npv": npv,
+        "f1": f1,
+        "auroc": auroc,
+        "auprc": auprc,
         "model": model,
     }
-    
+
     metrics_df = pd.DataFrame(metrics)
-        
+
     return metrics_df
 
-# %%
+
 if __name__ == "__main__":
     DATAPATH = "../../result/test"
     SAVEPATH = "../../result/bootstrapped"
-    
+
     total_result = []
-    
+
     df_orig = pd.read_csv(os.path.join(DATAPATH, "xgb_raw_test_result.csv"))
-    
+
     print("Conventional")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -163,11 +160,11 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='conventional',
     )
-    
+
     result.to_csv(os.path.join(SAVEPATH, "xgb_raw_results.csv"), index=False)
-    
+
     total_result.append(result)
-    
+
     print("XGBoost Raw")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -176,12 +173,13 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='xgb_raw',
     )
-    
+
     result.to_csv(os.path.join(SAVEPATH, "xgb_raw_results.csv"), index=False)
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "xgb_imputed_test_result.csv"))
-    
+
+    df_orig = pd.read_csv(os.path.join(
+        DATAPATH, "xgb_imputed_test_result.csv"))
+
     print("XGBoost Imputed")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -190,12 +188,13 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='xgb_imputed',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "xgb_imputed_results.csv"), index=False)
+
+    result.to_csv(os.path.join(
+        SAVEPATH, "xgb_imputed_results.csv"), index=False)
     total_result.append(result)
-    
+
     df_orig = pd.read_csv(os.path.join(DATAPATH, "xgb_fev_test_result.csv"))
-    
+
     print("XGBoost FEV")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -204,12 +203,13 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='xgb_fev',
     )
-    
+
     result.to_csv(os.path.join(SAVEPATH, "xgb_fev_results.csv"), index=False)
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "xgb_fev_fvc_test_result.csv"))
-    
+
+    df_orig = pd.read_csv(os.path.join(
+        DATAPATH, "xgb_fev_fvc_test_result.csv"))
+
     print("XGBoost FEV_FVC")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -218,11 +218,13 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='xgb_fev_fvc',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "xgb_fev_fvc_results.csv"), index=False)
+
+    result.to_csv(os.path.join(
+        SAVEPATH, "xgb_fev_fvc_results.csv"), index=False)
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "ann_imputed_test_result.csv"))
+
+    df_orig = pd.read_csv(os.path.join(
+        DATAPATH, "ann_imputed_test_result.csv"))
     print("ANN Imputed")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -231,10 +233,11 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='ann_imputed',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "ann_imputed_results.csv"), index=False)
+
+    result.to_csv(os.path.join(
+        SAVEPATH, "ann_imputed_results.csv"), index=False)
     total_result.append(result)
-    
+
     df_orig = pd.read_csv(os.path.join(DATAPATH, "ann_fev_test_result.csv"))
     print("ANN FEV")
     result = calculate_bootstrap_metric(
@@ -244,11 +247,12 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='ann_fev',
     )
-    
+
     result.to_csv(os.path.join(SAVEPATH, "ann_fev_results.csv"), index=False)
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "ann_fev_fvc_test_result.csv"))
+
+    df_orig = pd.read_csv(os.path.join(
+        DATAPATH, "ann_fev_fvc_test_result.csv"))
     print("ANN FEV_FVC")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -257,11 +261,13 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='ann_fev_fvc',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "ann_fev_fvc_results.csv"), index=False)
+
+    result.to_csv(os.path.join(
+        SAVEPATH, "ann_fev_fvc_results.csv"), index=False)
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "svm_imputed_test_result.csv"))
+
+    df_orig = pd.read_csv(os.path.join(
+        DATAPATH, "svm_imputed_test_result.csv"))
     print("SVM Imputed")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -270,10 +276,11 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='svm_imputed',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "svm_imputed_results.csv"), index=False)
+
+    result.to_csv(os.path.join(
+        SAVEPATH, "svm_imputed_results.csv"), index=False)
     total_result.append(result)
-    
+
     df_orig = pd.read_csv(os.path.join(DATAPATH, "svm_fev_test_result.csv"))
     print("SVM FEV")
     result = calculate_bootstrap_metric(
@@ -283,11 +290,16 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='svm_fev',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "svm_fev_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "svm_fev_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "svm_fev_fvc_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "svm_fev_fvc_test_result.csv")
+    )
     print("SVM FEV_FVC")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -296,13 +308,19 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='svm_fev_fvc',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "svm_fev_fvc_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "svm_fev_fvc_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "rf_imputed_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "rf_imputed_test_result.csv")
+    )
     df_orig = df_orig.assign(
-        pred_class = lambda x: np.where(x['pred_proba'] >= 0.526, 1, 0)
+        # pred_class = lambda x: np.where(x['pred_proba'] >= 0.526, 1, 0)
+        pred_class=lambda x: np.where(x['pred_proba'] >= 0.59, 1, 0)
     )
     print("Random Forest Imputed")
     result = calculate_bootstrap_metric(
@@ -312,11 +330,16 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='rf_imputed',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "rf_imputed_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "rf_imputed_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "rf_fev_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "rf_fev_test_result.csv")
+    )
     print("Random Forest FEV")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -325,11 +348,16 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='rf_fev',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "rf_fev_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "rf_fev_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "rf_fev_fvc_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "rf_fev_fvc_test_result.csv")
+    )
     print("Random Forest FEV_FVC")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -338,11 +366,16 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='rf_fev_fvc',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "rf_fev_fvc_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "rf_fev_fvc_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "logistic_imputed_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "logistic_imputed_test_result.csv")
+    )
     print("Logistic Imputed")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -351,11 +384,16 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='logistic_imputed',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "logistic_imputed_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "logistic_imputed_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "logistic_fev_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "logistic_fev_test_result.csv")
+    )
     print("Logistic FEV")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -364,11 +402,16 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='logistic_fev',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "logistic_fev_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "logistic_fev_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
-    df_orig = pd.read_csv(os.path.join(DATAPATH, "logistic_fev_fvc_test_result.csv"))
+
+    df_orig = pd.read_csv(
+        os.path.join(DATAPATH, "logistic_fev_fvc_test_result.csv")
+    )
     print("Logistic FEV_FVC")
     result = calculate_bootstrap_metric(
         df_log=df_orig,
@@ -377,10 +420,15 @@ if __name__ == "__main__":
         n_bootstrap=100,
         model='logistic_fev_fvc',
     )
-    
-    result.to_csv(os.path.join(SAVEPATH, "logistic_fev_fvc_results.csv"), index=False)
+
+    result.to_csv(
+        os.path.join(SAVEPATH, "logistic_fev_fvc_results.csv"), 
+        index=False
+    )
     total_result.append(result)
-    
+
     total_result_df = pd.concat(total_result, ignore_index=True)
-    total_result_df.to_csv(os.path.join(SAVEPATH, "total_results.csv"), index=False)
-# %%
+    total_result_df.to_csv(
+        os.path.join(SAVEPATH, "total_results.csv"), 
+        index=False
+    )
