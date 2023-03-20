8f296c01bf00fee427830427d7db69bf77970478
KyungHyun Jovinus Lee <lkh256@gmail.com>
2023-03-20 21:57:47 +0900

    change path to current setup

--- main/gridsearch_sklearn.sh
+++ main/gridsearch_sklearn.sh
@@ -1,14 +1,14 @@
-python gridsearch_sklearn.py --feature fev --model logistic --n_jobs 1
-python gridsearch_sklearn.py --feature fev --model rf --n_jobs 1
-python gridsearch_sklearn.py --feature fev --model svm --n_jobs -1
-python gridsearch_sklearn.py --feature fev --model ann --n_jobs 1
+python ./model/gridsearch_sklearn.py --feature fev --model logistic --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature fev --model rf --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature fev --model svm --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature fev --model ann --n_jobs -1
 
-python gridsearch_sklearn.py --feature fev_fvc --model logistic --n_jobs 1
-python gridsearch_sklearn.py --feature fev_fvc --model rf --n_jobs 1
-python gridsearch_sklearn.py --feature fev_fvc --model svm --n_jobs -1
-python gridsearch_sklearn.py --feature fev_fvc --model ann --n_jobs 1
+python ./model/gridsearch_sklearn.py --feature fev_fvc --model logistic --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature fev_fvc --model rf --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature fev_fvc --model svm --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature fev_fvc --model ann --n_jobs -1
 
-python gridsearch_sklearn.py --feature imputed --model logistic --n_jobs 1
-python gridsearch_sklearn.py --feature imputed --model rf --n_jobs 1
-python gridsearch_sklearn.py --feature imputed --model svm --n_jobs -1
-python gridsearch_sklearn.py --feature imputed --model ann --n_jobs 1
+python ./model/gridsearch_sklearn.py --feature imputed --model logistic --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature imputed --model rf --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature imputed --model svm --n_jobs -1
+python ./model/gridsearch_sklearn.py --feature imputed --model ann --n_jobs -1
